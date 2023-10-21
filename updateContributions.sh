574835083d06d00c59fccbebd0a9c00433e35db9
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2023-10-21 13:13:34 +0530

    Remove Bug

--- updateContributions.sh
+++ updateContributions.sh
@@ -3,5 +3,5 @@
 entireContributions=`python3 contributionDetails.py`
 echo $entireContributions
 
-awk -v var="$entireContributions" '{sub(/- Total Contributions in GitHub: **[0-9]+**/, "- Total Contributions in GitHub: **" var "**")} {print}' README.md > TEMP_README.md
+awk -v var="$entireContributions" '{sub(/- Total Contributions in GitHub: **[0-9]+/, "- Total Contributions in GitHub: **" var)} {print}' README.md > TEMP_README.md
 mv TEMP_README.md README.md
